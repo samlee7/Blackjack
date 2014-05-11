908ecf669862ab2e5fd26291c2632e18fe1b9a46
Sam Lee <slee103@ucr.edu>
2014-05-11 14:03:49 -0700

    added sleep + comments

--- blackjack.cpp
+++ blackjack.cpp
@@ -3,6 +3,7 @@
 #include <algorithm>
 #include <vector>
 #include <ctime>
+#include <windows.h>
 
 using namespace std;
 
@@ -18,6 +19,7 @@ struct card{
 	card(string suit, int val) : suit(suit), val(val) {}
 };
 
+/* creates a deck of size 52 */
 void create_deck(vector<card> &deck)
 {
 	for (int i = 1; i <= numval; i++)
@@ -40,17 +42,20 @@ void create_deck(vector<card> &deck)
 
 int main()
 {
+	cout << "Welcome to Sam's Blackjack" << endl << endl;
+
 	srand(unsigned(time(0)));
 	vector<card> deck;
 	create_deck(deck);
-	cout << deck.size() << endl;
 
+	/* shuffles the deck */
 	random_shuffle(deck.begin(), deck.end(), myrandom);
 
 	int cardamount = 0;
 	int totalValue = 0;
 	string input;
 
+	/* while loop that powers the game */
 	while (1)
 	{
 		while (cardamount < 2)
@@ -58,9 +63,12 @@ int main()
 			card top = deck.back();
 			deck.pop_back();
 
+			/* If the card value is > 10, it is set to 10 */
+			/* Jack, Queen, King has value of 10 */
 			if (top.val == 11 || top.val == 12 || top.val == 12) totalValue = totalValue + 10;
 			else totalValue = totalValue + top.val;
 
+			Sleep(2000);
 			cout << "Card showing: " << top.suit << " " << top.val << endl;
 			cardamount++;
 		}
@@ -74,9 +82,13 @@ int main()
 		cin >> input;
 
 		if (input == "no") break;
+		/* if input is yes, do nothing */
 		else if (input == "yes") ;
 
+		/* reset card value and total value */
 		cardamount = 0;
 		totalValue = 0;
+
+		cout << endl;
 	}
 }
